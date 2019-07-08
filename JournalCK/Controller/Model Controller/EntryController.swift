6b642959f23d28ee0e7c9e2f2f29226c60adbdee
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-07-08 17:48:54 -0600

    Built out the Model Controller

---
+++ JournalCK/Controller/Model Controller/EntryController.swift
@@ -0,0 +1,66 @@
+//
+//  EntryController.swift
+//  JournalCK
+//
+//  Created by Jason Mandozzi on 7/8/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class EntryController {
+    
+    //Source of Truth
+    var entries: [Entry] = []
+    
+    //CRUD
+    
+    //CREATE
+    
+    //save
+    //Our function takes in an Entry and completes with true when the Entry is saved
+    func saveEntry(entry: Entry, completion: @escaping (Bool) -> ()) {
+        //creating our entry that we're saving into a CKRecord using our convenience initializer from the model
+        let entryToSave = CKRecord(entry: entry)
+        CKContainer.default().privateCloudDatabase.save(entryToSave) { (record, error) in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) /n---/n \(error)")
+                completion(false)
+                return
+            }
+            guard let record = record,
+                let entry = Entry(record: record) else { completion(false) ; return}
+            self.entries.append(entry)
+            completion(true)
+        }
+    }
+    //create
+    func addEntryWith(title: String, body: String, completion: @escaping (Bool) -> Void) {
+        let entryToAdd = Entry(title: title, bodyText: body)
+        saveEntry(entry: entryToAdd, completion: completion)
+    }
+    
+    //Read
+    
+    func fetchEntries(completion: @escaping (Bool) -> ()) {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: EntryConstants.recordID, predicate: predicate)
+        CKContainer.default().privateCloudDatabase.perform(query, inZoneWith: nil) { (records, error) in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) /n---/n \(error)")
+                completion(false)
+                return
+            }
+            guard let records = records else { completion(false) ; return }
+            let entries = records.compactMap{ Entry(record: $0)}
+            self.entries = entries
+            completion(true)
+        }
+    }
+    
+    //Update
+    
+    //Delete
+    
+}
